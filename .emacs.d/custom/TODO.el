a4438386e8031df91a0a3d761ed052826766498c
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-04-02 11:21:40 +0900

    added todos

---
+++ .emacs.d/custom/TODO.el
@@ -0,0 +1,6 @@
+;; list of TODOs for my emacs:
+;; * write yasnippet for generating markdown-mode tables.
+;; * support racc, oedipus_lex and rubex.
+;; * support TODO, FIXME tags with highlighting.
+;; * automate generation of license/author banner at the start of files.
+;; * write yasnippet for generating YARD docs for ruby-mode.
