fbf1dcd7c7f54b7ce86db61c11b6e7dc52001069
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-04-18 16:59:19 +0900

    add yasnippet param snippet

--- .emacs.d/custom/TODO.el
+++ .emacs.d/custom/TODO.el
@@ -6,6 +6,9 @@
 ;; * write yasnippet for generating YARD docs for ruby-mode.
 ;; * running rspec properly from within emacs.
 ;; * yasnippet for generaring YARD params from function arguments.
-;; * yasnippet for @params in YARD docs.
 ;; * yasnippet for making links in org-mode.
 ;; * which-function-mode shows up in both top and bottom. make it top only.
+;; * When switching to *compilation* buffer change default encoding to utf-8. This link: http://ergoemacs.org/emacs/emacs_encoding_decoding_faq.html
+;; * Setup org-mode to allow selection with shift key.
+;; * The dashboard should be the first screen to be displayed.
+;; * org mode TODOs should be displayed on the dashboard.
