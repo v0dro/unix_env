a02b3b5df895e45ceace8bc97ddd44fe0bffe0c1
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-04-27 19:32:08 +0900

    hippie expand

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -172,4 +172,15 @@
 (setq default-input-method "japanese-mozc")
 (setq mozc-candidate-style 'overlay)
 
-
+;; hippie expand completions list
+(setq hippie-expand-try-functions-list '(
+                                         try-expand-dabbrev
+                                         try-expand-dabbrev-all-buffers
+                                         try-expand-dabbrev-from-kill
+                                         try-complete-file-name-partially
+                                         try-complete-file-name
+                                         try-expand-all-abbrevs
+                                         try-expand-list
+                                         try-expand-line
+                                         try-complete-lisp-symbol-partially
+                                         try-complete-lisp-symbol))
