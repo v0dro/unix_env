894b99a9c233709a1e5ad45eab5ae49481716992
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-05-02 14:20:20 +0900

    customize dashboard

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -136,11 +136,9 @@
   :config
   (dashboard-setup-startup-hook))
 
-(setq dashboard-items '((recents  . 5)
-                        (bookmarks . 5)
-                        (projects . 5)
-                        (agenda . 5)
-                        (registers . 5)))
+(setq dashboard-items '((recents  . 10)
+                        (projects . 10)
+                        (agenda . 5)))
 
 ;; YARD mode config
 (require 'yard-mode)
