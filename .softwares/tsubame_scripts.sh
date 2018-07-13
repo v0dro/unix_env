da6203d4552687769ec01c71c973612c99182f82
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-07-13 20:03:13 +0900

    more functions

--- .softwares/tsubame_scripts.sh
+++ .softwares/tsubame_scripts.sh
@@ -5,3 +5,13 @@ GROUP_NAME="jh160041"
 interactive() {
     qrsh -g $GROUP_NAME -l q_node=1 -l h_rt=$1 -pty yes -display $DISPLAY -v xterm /bin/bash
 }
+
+module_load() {
+    module load intel intel-mpi allinea
+}
+
+session_setup() {
+    . /etc/profile.d/modules.sh
+    export TERM=xterm
+    module_load
+}
