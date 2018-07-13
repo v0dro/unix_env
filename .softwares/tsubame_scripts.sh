1d507bf0ca33356e7d76011f378eabdf0575070b
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-07-13 16:32:06 +0900

    tsubame scripts

--- .softwares/tsubame_scripts.sh
+++ .softwares/tsubame_scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-$GROUP_NAME = "jh160041"
+GROUP_NAME="jh160041"
 
 # Args: $1 - time in format HH:MM:SS
 interactive() {
-    qrsh -g $GROUP_NAME -l q_node=1 -l h_rt=$1 -pty yes -display $DISPLAY -v TERM /bin/bash
+    qrsh -g $GROUP_NAME -l q_node=1 -l h_rt=$1 -pty yes -display $DISPLAY -v xterm /bin/bash
 }
