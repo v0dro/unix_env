20811ae2e260eb000cc29ce899eaf174e89e5b64
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-11-24 20:22:28 +0900

    enh ruby mode and rvm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -224,3 +224,18 @@
 (require 'reftex)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex) ; with AUCTeX LaTeX mode
 (setq reftex-plug-into-AUCTeX t) ; Anleitung S. 4
+
+;; enh-ruby-mode
+(add-hook 'enh-ruby-mode-hook 'robe-mode)
+(add-hook 'enh-ruby-mode-hook 'yard-mode)
+
+(add-to-list 'auto-mode-alist
+             '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+
+;; robe mode
+(require 'rvm)
+(rvm-use-default) ;; use rvm's default ruby for the current Emacs session
+
+(defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
+  (rvm-activate-corresponding-ruby))
+
