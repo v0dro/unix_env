b2f5664637c29ab31c2ebe1796e2b4a5b8e893df
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2020-04-10 10:34:21 +0900

    update pytorch scripts

--- scripts/pytorch-setup.sh
+++ scripts/pytorch-setup.sh
@@ -23,7 +23,9 @@ pytorch_do_pre_setup() {
 
 # Update pytorch submodules from upstream.
 pytorch_update_modules() {
-    . /usr/local/cuda/env.sh
+    if test -f /usr/local/cuda/env.sh; then
+        . /usr/local/cuda/env.sh
+    fi
     git pull --rebase
     git submodule sync --recursive
     git submodule update --init --recursive    
@@ -31,15 +33,20 @@ pytorch_update_modules() {
 
 # Prepare environment.
 pytorch_setup_env() {
+    export USE_CUDA=0
     export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
     export CXXFLAGS="`echo $CXXFLAGS | sed 's/-std=c++17/-std=c++14/'`"
     export CFLAGS="$CFLAGS -L$CONDA_PREFIX/lib"
-    export CXXFLAGS="$CXXFLAGS -L$CONDA_PREFIX/lib -L$CUDA_HOME/lib64 -lcusparse -lcublas "
+    export CXXFLAGS="$CXXFLAGS -L$CONDA_PREFIX/lib "
     export USE_NCCL=0
-    export USE_CUDA=1
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:$CUDA_HOME/lib64"
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath,${CUDA_HOME}/lib64 -Wl,-rpath-link,${CUDA_HOME}/lib64 -L${CUDA_HOME}/lib64"
-    export MAX_JOBS=20    
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu"
+    export MAX_JOBS=20
+
+    if [ "$USE_CUDA" -eq "1" ]; then
+        export CXXFLAGS="$CXXFLAGS -L$CUDA_HOME/lib64 -lcusparse -lcublas "
+        export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_HOME/lib64"
+        export LDFLAGS="${LDFLAGS} -Wl,-rpath,${CUDA_HOME}/lib64 -Wl,-rpath-link,${CUDA_HOME}/lib64 -L${CUDA_HOME}/lib64"
+    fi
 }
 
 pytorch_install() {
@@ -79,12 +86,4 @@ pytorch_setup_cuda() {
     pytorch_update_modules
     print_environment
     pytorch_install
-}
-
-pytorch_setup_cuda() {
-    
-}
-
-pytorch_check_linting() {
-    
 }
