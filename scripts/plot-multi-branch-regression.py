ce317b9767b9d950556f51bfcc791b2451bceca1
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2020-05-15 15:09:15 +0900

    plot script done

--- scripts/plot-multi-branch-regression.py
+++ scripts/plot-multi-branch-regression.py
@@ -16,6 +16,7 @@ def run_program(files):
             branches = csv['branch'].unique()
             bar_width = 1 / (len(branches)+1)
             num_test_cases = len(csv[csv['branch'] == branches[0]])
+            uniq_test_cases = csv['test_case'].unique()
             x_vals = np.arange(num_test_cases)
             
             for branch in branches:
@@ -23,13 +24,13 @@ def run_program(files):
                 plt.bar(x_vals, mean_time, width=bar_width, label=branch)
                 x_vals = [x + bar_width for x in x_vals]
 
+            plt.xticks([r for r in range(len(uniq_test_cases))], uniq_test_cases)
             plt.title(f)
             plt.xlabel("test case")
             plt.ylabel("time")
             plt.legend()
-            # plt.savefig(f+".pdf")
+            plt.savefig(full_file + ".pdf")
             plt.show()
-                
 
 if __name__ == "__main__":
     run_program()
