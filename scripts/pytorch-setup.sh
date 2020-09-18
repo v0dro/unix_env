7ac94e93a8793c33e547ea6364440d01ccfff8ea
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2020-09-17 22:57:06 -0500

    pytorch script update

--- scripts/pytorch-setup.sh
+++ scripts/pytorch-setup.sh
@@ -23,16 +23,29 @@ pytorch_do_pre_setup() {
 
 # Update pytorch submodules from upstream.
 pytorch_update_modules() {
-    if test -f /usr/local/cuda/env.sh; then
-        . /usr/local/cuda/env.sh
-    fi
     git pull --rebase
     git submodule sync --recursive
     git submodule update --init --recursive    
 }
 
 # Prepare environment.
+# pytorch_setup_env USE_CUDA
 pytorch_setup_env() {
+    export DEBUG=1
+    export USE_CUDA=1
+    export USE_NCCL=0
+    export USE_NCCL=0
+    export MAX_JOBS=20
+    export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+    export CXXFLAGS="`echo $CXXFLAGS | sed 's/-std=c++17/-std=c++14/'`"
+    export CFLAGS="$CFLAGS -L$CONDA_PREFIX/lib"
+    export CXXFLAGS="$CXXFLAGS -L$CONDA_PREFIX/lib "
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu"
+
+    if test -f /usr/local/cuda/env.sh; then
+        . /usr/local/cuda/env.sh
+    fi
+    
     if [[ "$WITH_CLANG" -eq "1" ]]; then
         export CC=${CONDA_PREFIX}/bin/clang
         export CXX=${CONDA_PREFIX}/bin/clang++
@@ -45,21 +58,15 @@ pytorch_setup_env() {
         export CXX=/usr/bin/g++
     fi
 
-    export DEBUG=1
-    export USE_CUDA=0
-    export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-    export CXXFLAGS="`echo $CXXFLAGS | sed 's/-std=c++17/-std=c++14/'`"
-    export CFLAGS="$CFLAGS -L$CONDA_PREFIX/lib"
-    export CXXFLAGS="$CXXFLAGS -L$CONDA_PREFIX/lib "
-    export USE_NCCL=0
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu"
-    export MAX_JOBS=20
-
     if [ "$USE_CUDA" -eq "1" ]; then
-        export CXXFLAGS="$CXXFLAGS -L$CUDA_HOME/lib64 -lcusparse -lcublas "
-        export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_HOME/lib64"
-        export LDFLAGS="${LDFLAGS} -Wl,-rpath,${CUDA_HOME}/lib64 -Wl,-rpath-link,${CUDA_HOME}/lib64 -L${CUDA_HOME}/lib64"
+        export CXXFLAGS="$CXXFLAGS -L${CUDA_HOME}/lib64 -lcusparse -lcublas "
+        export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${CUDA_HOME}/lib64"
+        export LDFLAGS="${LDFLAGS} -Wl,-rpath,${CUDA_HOME}/lib64 -Wl,-rpath-link,${CUDA_HOME}/lib64 -L${CUDA_HOME}/lib64 -lrt -lpthread -lresolv "
     fi
+
+    export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
+    python setup.py build --cmake-only
+    ccmake build  # or cmake-gui build
 }
 
 pytorch_install() {
